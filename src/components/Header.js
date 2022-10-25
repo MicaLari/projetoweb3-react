9fd301b195c62887f5dbb0a20e3b71508c5e04fc
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-10-25 17:39:47 -0300

    Creat de Link and ClassName

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import './Header.css'
+import {GiFilmSpool as Film} from 'react-icons/gi'
+import { NavLink, Link } from 'react-router-dom'
+import { BsStar as Estrela } from "react-icons/bs";
+// import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className='container'>
+          <Link to='/'>
+            <div className='logotype'>
+              <Film className='icon-logo'/>
+              <h1>FreeMovies</h1>
+            </div>
+          </Link>
+          <nav>
+            <ul>
+              
+              <li><NavLink to='/filmes' end>Filmes</NavLink></li>
+              <li><NavLink to='/series' end>Series</NavLink></li>
+              <li><Estrela className='icon-type'/>   <NavLink to='/lancamentos' end>Lançamentos</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+    </div>
+    
+  )
+}
+
+
+
+export default Header
