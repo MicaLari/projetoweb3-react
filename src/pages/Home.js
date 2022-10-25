9db31e7ed53035d708801e3eac37ab62cf01a403
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-10-25 17:39:03 -0300

    Creat da const

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import Header from "../components/Header"
+import "./Home.css"
+import MainContainer from '../components/MainContainer'
+
+
+const Home = () => {
+
+  return (
+    <>
+      <Header />
+
+      <MainContainer>
+        <h1>Home</h1>
+        <p>Lista usuários API Git Hub:</p>
+
+      </MainContainer>
+
+  
+    </> 
+  )
+}
+
+
+
+export default Home
