4820f9e6f5890b5867d8eb64139cc3e07c12d003
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-10-25 17:38:15 -0300

    Creat do Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='main-footer'>
+      <p>© 2022 Micaella Larissa</p>
+    </footer>
+  )
+}
+
+export default Footer
