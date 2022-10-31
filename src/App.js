2f5ebed46c433ec291fba794c87bbc92d9ac559f
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-10-31 17:49:16 -0300

    Import de /login and /cadastro

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home'
 import Films from './pages/Films'
 import Series from './pages/Series'
 import Lancamentos from './pages/Lancamentos'
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
 
 
 
@@ -13,6 +15,8 @@ const App = () => {
       <Route path='/filmes' element={<Films />} />
       <Route path='/series' element={<Series />} />
       <Route path='/lancamentos' element={<Lancamentos />} />
+      <Route path='/Login' element={<Login/>} />
+      <Route path='/Cadastro' element={<Cadastro/>}/>
     </Routes>
   );
 }
