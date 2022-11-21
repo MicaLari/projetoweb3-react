ea878fb15179076e904f43bd9cf41a356135de1f
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-11-21 17:42:17 -0300

    add do Sobre

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,9 +38,14 @@ const Home = () => {
           </Carousel.Item>
 
         </Carousel>
+        <br />
 
-        <h1 className="test"> Sobre o Site </h1>
-        <p>Lista usuários API Git Hub:</p>
+        <h1> Sobre o Site </h1>
+        <p>It is a long established fact that a reader will be distracted by the readable 
+          content of a page when looking at its layout.
+          The point of using Lorem Ipsum is that it has a more-or-less normal distribution 
+          of letters, as opposed to using 'Content here, content here', making it look like readable English.
+        </p>
 
       </MainContainer>
 
