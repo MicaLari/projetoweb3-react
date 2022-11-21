148f0f569b90c914a3726936e5f41197719e6e5d
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-11-21 17:25:02 -0300

    Creat of Carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import "./Home.css"
 import MainContainer from '../components/MainContainer'
+import Carousel from 'react-bootstrap/Carousel';
 
 
 const Home = () => {
@@ -9,14 +10,42 @@ const Home = () => {
     <>
       <Header />
 
+
       <MainContainer>
-        <h1>Home</h1>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="assets/img/filme1.jpeg"
+              alt="Minios"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="assets/img/filme2.jpeg"
+              alt="Thor: Amor e trovão"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="assets/img/filme3.jpg"
+              alt="sla"
+            />
+          </Carousel.Item>
+
+        </Carousel>
+
+        <h1 className="test"> Sobre o Site </h1>
         <p>Lista usuários API Git Hub:</p>
 
       </MainContainer>
 
-  
-    </> 
+
+    </>
   )
 }
 
