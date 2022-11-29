16399ed1ae7175fc4d5b06473c46708446b69a0e
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-11-29 17:00:54 -0300

    add de Route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Lancamentos from './pages/Lancamentos'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeAdmin from './pages/HomeAdmin';
+import LayoutAdmin from './pages/LayoutAdmin';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -18,7 +21,16 @@ const App = () => {
         <Route path='/lancamentos' element={<Lancamentos />} />
         <Route path='/Login' element={<Login/>} />
         <Route path='/Cadastro' element={<Cadastro/>}/>
+
+        <Route path='/admin' element={<LayoutAdmin />}>
+          <Route index element={<HomeAdmin />}/>
+        </Route>
+      
+        <Route path='*' element={<NotFound />} status={404} />
+
       </Routes>
+
+     
 
     </AuthProvider>
    
