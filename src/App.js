916c01adee3af8ee0344527801aa01a8023aed06
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-11-29 17:41:44 -0300

    import Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
-import AuthProvider from './Providers/AuthProvider';
+import AuthProvider from './Providers/AuthProvider'
 import Home from './pages/Home'
 import Films from './pages/Films'
 import Series from './pages/Series'
 import Lancamentos from './pages/Lancamentos'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HomeAdmin from './pages/HomeAdmin';
-import LayoutAdmin from './pages/LayoutAdmin';
-import NotFound from './pages/NotFound';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import HomeAdmin from './pages/HomeAdmin'
+import LayoutAdmin from './pages/LayoutAdmin'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
@@ -22,9 +22,8 @@ const App = () => {
         <Route path='/Login' element={<Login/>} />
         <Route path='/Cadastro' element={<Cadastro/>}/>
 
-        <Route path='/admin' element={<LayoutAdmin />}>
-          <Route index element={<HomeAdmin />}/>
-        </Route>
+        <Route path='/admin' element={<HomeAdmin />}/>
+
       
         <Route path='*' element={<NotFound />} status={404} />
 
